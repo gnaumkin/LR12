74dfcacb7487578bb59447fd66680ccdc627dbd4
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-12-27 11:42:45 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,22 @@
+﻿var
+  F_in,F_out: Text;
+  S,a: String;
+begin
+ Write('S: ');
+ Readln(S);
+ Assign(F_in,'C:\Users\пользователь\Desktop\лабы\лр12\text3.txt');
+ Reset(F_in);
+ Assign(F_out,'C:\Users\пользователь\Desktop\лабы\лр12\text3.1.txt');
+ Rewrite(F_out);
+ While not eof(F_in) do
+  begin
+   Readln(F_in,a);
+   Writeln(F_out,a);
+  end;
+  Rewrite(F_out);
+ Writeln(F_out,S);
+ Close(F_in);
+ Close(F_out);
+ Erase(F_in);
+ Rename(F_out,'C:\Users\пользователь\Desktop\лабы\лр12\text3.txt');
+end.
